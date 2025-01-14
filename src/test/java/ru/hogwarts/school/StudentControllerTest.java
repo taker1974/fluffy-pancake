34c341cc22d4e6db0546c4021b81d46d0b4dd6b8
taker1974 <kostus.online.1974@yandex.ru>
2025-01-14 12:01:21 +0300

    подготовка к тестам

--- src/test/java/ru/hogwarts/school/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/StudentControllerTest.java
@@ -11,14 +11,23 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 /**
- * StudentServiceTest.
+ * StudentControllerTest.<br>
+ * <p>
+ * Из условий ДЗ:<br>
+ * Шаг 1<br>
+ * Создать класс для тестирования в пакете test.<br>
+ * Создать по одному тесту на каждый эндпоинт контроллера StudentController, используя TestRestTemplate.<br>
+ * Критерии оценки:<br>
+ * - в пакете test создан класс для тестирования StudentController;<br>
+ * - для тестирования использовался TestRestTemplate;<br>
+ * - для каждого эндпоинта контроллера StudentController создан как минимум один тест.<br>
  *
  * @author Константин Терских, kostus.online.1974@yandex.ru, 2025
  * @version 0.2
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-class StudentServiceTest {
+class StudentControllerTest {
 
     @LocalServerPort
     private int port;
