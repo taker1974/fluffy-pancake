fa1328aa0512c9c0eff39dfa3a77fb970836cf2a
taker1974 <kostus.online.1974@yandex.ru>
2025-02-08 13:24:39 +0300

    ДЗ "Параллельные стримы" выполнено

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 
 import java.util.List;
 
@@ -13,4 +14,7 @@ public interface FacultyRepository extends JpaRepository<Faculty, Long> {
     @Query(value = "SELECT f FROM faculty f WHERE LOWER(f.name) = LOWER(:name) OR LOWER(f.color) = LOWER(:color)",
             nativeQuery = true)
     List<Faculty> findByNameOrColorIgnoreCase(String name, String color);
+
+    @Query(value = "SELECT * FROM faculty f WHERE LOWER(f.name) LIKE LOWER(%:suffix)", nativeQuery = true)
+    List<Faculty> findByNameSuffix(String suffix);
 }
